e240be9a08f478aa987fd698e2e0fa50d5b7bdb8
moifouss <jsfousseni@hotmail.com>
2014-12-19 14:57:18 +0000

    Version 1

---
+++ WpfLogin/Model/Authentification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLogin.Model
+{
+    class Authentification
+    {
+        public static bool Authentifier(string Compte, string ModeDePasse)
+        {
+            //On doit faire une requette ici qui retourne la valeur true ou false
+            return true; 
+        }
+    }
+}
---
+++ WpfLogin/View/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfLogin.Model;
+
+namespace WpfLogin.View
+{
+    /// <summary>
+    /// Logique d'interaction pour Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+         
+        {
+            if (Model.Authentification.Authentifier(txtCompte.Text, txtMotDePasse.ToString()))
+            {
+                this.Hide();
+                MainWindow fenPrincipale = new MainWindow();
+                fenPrincipale.Show();
+               
+            }
+           
+        }
+        
+
+        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
